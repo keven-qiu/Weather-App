dbeef1152db414516a7c0c85727330d10bab55d7
Keven Qiu <qiukeven@icloud.com>
2021-05-16 12:55:26 -0600

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Keven Qiu on 2021-05-16.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
